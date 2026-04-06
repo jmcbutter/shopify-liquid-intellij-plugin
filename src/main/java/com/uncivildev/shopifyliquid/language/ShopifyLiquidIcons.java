3286cbef63ea2b70f210335b3cd408cfe38b45fa
Jordan Butterfield <45950703+jmcbutter@users.noreply.github.com>
2026-04-06 12:12:15 -0500

    Initial Commit: Tutorial Steps 1 & 2

---
+++ src/main/java/com/uncivildev/shopifyliquid/language/ShopifyLiquidIcons.java
@@ -0,0 +1,8 @@
+package com.uncivildev.shopifyliquid.language;
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class ShopifyLiquidIcons {
+    public static final Icon FILE = IconLoader.getIcon("/icons/shopify_logo-16x16.png", ShopifyLiquidIcons.class);
+}
---
+++ src/main/java/com/uncivildev/shopifyliquid/language/ShopifyLiquidLanguage.java
@@ -0,0 +1,10 @@
+package com.uncivildev.shopifyliquid.language;
+import com.intellij.lang.Language;
+
+public class ShopifyLiquidLanguage extends Language {
+    public static final ShopifyLiquidLanguage INTANCE = new ShopifyLiquidLanguage();
+
+    private ShopifyLiquidLanguage() {
+        super("ShopifyLiquid");
+    }
+}
